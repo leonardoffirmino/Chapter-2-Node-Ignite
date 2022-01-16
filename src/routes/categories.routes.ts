b509b7fe5d59ef226a9fc63cfcb7871cc91aa942
leonardoffirmino <firmino750@gmail.com>
2022-01-16 20:53:44 -0300

    Refactory listCategory

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
-import { CategoriesRepository } from "../modules/cars/repositories/CategoriesRepository";
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
+import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
 
 
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
+
 
 
 categoriesRoutes.post("/", (request, response) => {
@@ -16,9 +16,7 @@ categoriesRoutes.post("/", (request, response) => {
 });
 
 categoriesRoutes.get("/", (request, response) => {
-  const all = categoriesRepository.list();
-
-  return response.json(all);
+  return listCategoriesController.handle(request, response);
 })
 
 export { categoriesRoutes };
