ced6e9d5ec362feea6d0edb28bc5e297fc17e367
leonardoffirmino <firmino750@gmail.com>
2022-01-17 23:58:25 -0300

    Add multer aplication

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 
+import multer from "multer";
+
 import { createCategoryController } from '../modules/cars/useCases/createCategory';
 import { listCategoriesController } from '../modules/cars/useCases/listCategories';
 
@@ -8,7 +10,9 @@ import { listCategoriesController } from '../modules/cars/useCases/listCategorie
 
 const categoriesRoutes = Router();
 
-
+const upload = multer({
+  dest: "./tmp",
+})
 
 categoriesRoutes.post("/", (request, response) => {
   return createCategoryController.handle(request, response);
@@ -17,6 +21,15 @@ categoriesRoutes.post("/", (request, response) => {
 
 categoriesRoutes.get("/", (request, response) => {
   return listCategoriesController.handle(request, response);
+})
+
+
+categoriesRoutes.post("/import", upload.single("file"), (request, response) => {
+  const { file } = request;
+
+  console.log(file);
+
+  return response.send();
 })
 
 export { categoriesRoutes };
