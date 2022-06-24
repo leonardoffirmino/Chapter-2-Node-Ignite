5a1b2bc050b01fce51ecbae74bc2876fd8cbc948
leonardoffirmino <firmino750@gmail.com>
2022-06-23 22:24:11 -0300

    add postgres db

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,5 +1,5 @@
 
-import { Category } from "../../model/Category";
+import { Category } from "../../entities/Category";
 import { ICategoriesRepository } from "../../repositories/ICategoriesRepository";
 
 class ListCategoriesUseCase {
