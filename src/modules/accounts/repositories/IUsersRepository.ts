3b7224c82691d3b6ac4c9e07121e4d7caf6078ae
leonardoffirmino <firmino750@gmail.com>
2022-07-13 22:38:37 -0300

    Create repository user

---
+++ src/modules/accounts/repositories/IUsersRepository.ts
@@ -0,0 +1,10 @@
+import { ICreateUserDTO } from "../dtos/ICreateUserDTO";
+
+
+
+interface IUsersRepository {
+  create(data: ICreateUserDTO): Promise<void>;
+}
+
+
+export { IUsersRepository }
---
+++ src/modules/accounts/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,32 @@
+import { getRepository, Repository } from "typeorm";
+
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+
+class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+
+
+  async create({ name, email, username, password, driver_license }: ICreateUserDTO): Promise<void> {
+    const user = this.repository.create({
+      name,
+      email,
+      password,
+      driver_license,
+      username
+    });
+
+    await this.repository.save(user);
+  }
+
+}
+
+
+export { UsersRepository }
