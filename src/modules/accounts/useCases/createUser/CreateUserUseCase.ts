c32ffb14d87f4474b0dedae5c8436a199c7eb604
leonardoffirmino <firmino750@gmail.com>
2022-07-14 22:39:45 -0300

    Create usecase Users

---
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -0,0 +1,30 @@
+import { inject } from "tsyringe";
+import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+
+import { IUsersRepository } from "../../repositories/IUsersRepository";
+
+
+
+
+class CreateUserUseCase {
+  constructor(
+    @inject("UsersRepository")
+    private usersRepository: IUsersRepository
+  ) { }
+
+
+
+
+  async execute({ name, email, username, driver_license, password }: ICreateUserDTO): Promise<void> {
+    await this.usersRepository.create({
+      name,
+      email,
+      username,
+      driver_license,
+      password
+    })
+  }
+}
+
+
+export { CreateUserUseCase }
--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,6 +5,9 @@ import { CategoriesRepository } from "../../modules/cars/repositories/implementa
 import { SpecificationsRepository } from "../../modules/cars/repositories/implementations/SpecificationsRepository";
 import { ISpecificationsRepository } from "../../modules/cars/repositories/ISpecificatiosRepository";
 
+import { IUsersRepository } from "../../modules/accounts/repositories/IUsersRepository";
+import { UsersRepository } from "../../modules/accounts/repositories/implementations/UsersRepository";
+
 container.registerSingleton<ICategoriesRepository>(
   "CategoriesRepository",
   CategoriesRepository
@@ -14,3 +17,9 @@ container.registerSingleton<ISpecificationsRepository>(
   "SpecificationsRepository",
   SpecificationsRepository
 );
+
+
+container.registerSingleton<IUsersRepository>(
+  "UsersRepository",
+  UsersRepository
+)
