31b2e0d1ff8f5e56a7b74efae2fe1427998e7733
leonardoffirmino <firmino750@gmail.com>
2022-07-21 21:53:03 -0300

    Add bcrypt password

--- src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/useCases/createUser/CreateUserUseCase.ts
@@ -1,5 +1,6 @@
 import { inject, injectable } from "tsyringe";
 import { ICreateUserDTO } from "../../dtos/ICreateUserDTO";
+import { hash } from "bcryptjs";
 
 import { IUsersRepository } from "../../repositories/IUsersRepository";
 
@@ -16,11 +17,21 @@ class CreateUserUseCase {
 
 
   async execute({ name, email, driver_license, password }: ICreateUserDTO): Promise<void> {
+
+    const userAlreadyExists = await this.usersRepository.findByEmail(email);
+
+    if (userAlreadyExists) {
+      throw new Error("User already exists");
+    }
+
+    const passwordHash = await hash(password, 8);
+
+
     await this.usersRepository.create({
       name,
       email,
       driver_license,
-      password
+      password: passwordHash
     })
   }
 }
