15606753a2c2cf54ae772067c28b012041a43202
leonardoffirmino <firmino750@gmail.com>
2023-04-20 22:35:06 -0300

    Create Seed

---
+++ src/shared/infra/typeorm/seed/admin.ts
@@ -0,0 +1,18 @@
+import { getConnection } from "typeorm";
+import { v4 as uuidV4 } from "uuid";
+import { hash } from "bcryptjs";
+
+
+
+async function create() {
+  const connection = getConnection();
+
+  const id = uuidV4();
+  const password = await hash("admin", 8);
+
+
+  await connection.query(
+    `INSERT INTO USERS(id,name,email,password,admin,created_at)
+      values('${id}','admin','admin@rentx.com.br', '${password}',true,${new Date()})`
+  )
+}
