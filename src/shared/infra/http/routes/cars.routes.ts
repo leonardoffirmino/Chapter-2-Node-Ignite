558635efd8952c2982cd268866cd1274a19804ca
leonardoffirmino <firmino750@gmail.com>
2023-06-05 23:33:32 -0300

    create upload images

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -4,6 +4,11 @@ import { ensureAuthenticate } from "../middlewares/ensureAuthenticate";
 import { ensureAdmin } from "../middlewares/ensureAdmin";
 import { ListAvailableCarsController } from "@modules/cars/useCases/listAvailableCars/ListAvailableCarsController";
 import { CreateCarSpecificationController } from "@modules/cars/useCases/createCarSpecification/CreateCarSpecificationController";
+import { UploadCarImagesController } from "@modules/cars/useCases/uploadCarImages/UploadCarImagesController";
+
+import uploadConfig from "@config/upload";
+import multer from "multer";
+
 
 
 const carsRoutes = Router();
@@ -12,6 +17,10 @@ const carsRoutes = Router();
 const createCarController = new CreateCarController();
 const listAvailableCarsController = new ListAvailableCarsController();
 const createCarSpecificationController = new CreateCarSpecificationController();
+const uploadCarImagesController = new UploadCarImagesController();
+
+
+const upload = multer(uploadConfig.upload("./tmp/cars"));
 
 
 
@@ -20,6 +29,8 @@ carsRoutes.post("/", ensureAuthenticate, ensureAdmin, createCarController.handle
 carsRoutes.get("/available", listAvailableCarsController.handle);
 
 carsRoutes.post("/specifications/:id", ensureAuthenticate, ensureAdmin, createCarSpecificationController.handle);
+
+carsRoutes.post("/images", ensureAuthenticate, ensureAdmin, upload.array("images"), uploadCarImagesController.handle);
 
 
 export { carsRoutes };
