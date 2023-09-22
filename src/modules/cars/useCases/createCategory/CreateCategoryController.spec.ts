d6d8f7cef10980ae267305380160f3198b347ec8
leonardoffirmino <firmino750@gmail.com>
2023-09-21 21:55:20 -0300

    Create controller supertest

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -5,7 +5,15 @@ import request from "supertest";
 
 
 describe("Create Category Controller", async () => {
-  it("Test", async () => {
-    await request(app).get("/cars/available").expect(200);
+  it("should be able to create a new category", async () => {
+
+    const response = await request(app).post("/categories").send({
+      name: "Category Supertest",
+      description: "Category Supertest"
+    });
+
+
+    expect(response.status).toBe(201);
+
   });
 });
