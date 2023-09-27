2914320922f01b866d81aa4845a66e718990e0e1
leonardoffirmino <firmino750@gmail.com>
2023-09-26 22:46:51 -0300

    Create connection database qa

--- src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryController.spec.ts
@@ -1,16 +1,53 @@
 import request from "supertest";
 
 import { app } from "@shared/infra/http/app";
+import { v4 as uuid } from "uuid";
+
+import createConnection from "@shared/infra/typeorm";
+import { Connection } from "typeorm";
+import { hash } from "bcryptjs";
 
 
+let connection: Connection;
 
 
-describe("Create Category Controller", async () => {
+describe("Create Category Controller", () => {
+
+  beforeAll(async () => {
+    connection = await createConnection();
+    await connection.runMigrations();
+
+    const id = uuid();
+    const password = await hash("admin", 8);
+
+
+    (await connection).query(
+      `INSERT INTO USERS(id,name,email,password,"isAdmin",created_at,driver_license)
+      values('${id}','admin','admin@rentx.com.br', '${password}',true,'now()', 'XXXX-XXX')`
+    )
+  });
+
+
+  afterAll(async () => {
+    await connection.dropDatabase();
+    await connection.close();
+  });
+
   it("should be able to create a new category", async () => {
+
+    const responseToken = await request(app).post("/categories").send({
+      email: "admin@rentx.com.br",
+      password: "admin"
+    });
+
+
+    const { token } = responseToken.body;
 
     const response = await request(app).post("/categories").send({
       name: "Category Supertest",
       description: "Category Supertest"
+    }).set({
+      Authorization: `Bearer ${token}`
     });
 
 
